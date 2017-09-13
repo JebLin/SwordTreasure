99b31e71ba3a249f46851981e3b0ab229c0ff8f1
JebLin <sword52888@163.com>
2017-09-13 08:07:11 +0800

    Lambda

---
+++ SwordUtil/src/test/java/indi/sword/util/jdk8/lambda/_02_MyFun.java
@@ -0,0 +1,10 @@
+package indi.sword.util.jdk8.lambda;
+
+/**
+ * Created by rd_jianbin_lin on 2017/9/13.
+ */
+@FunctionalInterface
+public interface _02_MyFun {
+
+    public Integer getValue(Integer num);
+}
--- SwordUtil/src/test/java/indi/sword/util/jdk8/lambda/_02_TestLambda.java
+++ SwordUtil/src/test/java/indi/sword/util/jdk8/lambda/_02_TestLambda.java
@@ -1,7 +1,9 @@
 package indi.sword.util.jdk8.lambda;
 
+import java.util.*;
 import java.util.function.Consumer;
 
+import com.sun.org.apache.xpath.internal.SourceTree;
 import org.junit.Test;
 
 public class _02_TestLambda {
@@ -31,5 +33,44 @@ public class _02_TestLambda {
 		Consumer<String> con = x -> System.out.println(x);
 		con.accept("国泰平安");
 	}
+
+	@Test
+	public void test3(){
+		Comparator<Integer> com = (x, y) -> {
+			System.out.println("函数式接口");
+			return Integer.compare(x,y);
+		};
+	}
+
+
+	@Test
+	public void test4(){
+		Comparator<Integer> com = (x,y) -> Integer.compare(x,y);
+	}
+
+	@Test
+	public void test5(){
+//		String[] strs;
+//		strs = {"aaa", "bbb", "ccc"};
+
+		List<String> list = new ArrayList<>();
+
+		show(new HashMap<>()); // 可以这样传入泛型Map
+	}
+
+	public void show(Map<String, Integer> map){
+
+	}
 	
+	//需求：对一个数进行运算
+	@Test
+	public void test6(){
+		Integer sum  = operation(100, (x) -> x * x);
+		System.out.println(sum);
+		System.out.println(operation(100, (x) -> x + x));
+	}
+
+	public Integer operation(Integer sum,_02_MyFun myFun){
+		return myFun.getValue(sum);
+	}
 }
