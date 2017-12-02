565f75a497ad08b352a9643580aaa3d8803ed4a9
JebLin <Sword52888@163.com>
2017-12-02 11:46:51 +0800

    reentrantlock

---
+++ SwordUtil/src/test/java/indi/sword/util/concurrent/_08_01_TestReentrantLock.java
@@ -0,0 +1,36 @@
+package indi.sword.util.concurrent;
+
+
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+/**
+ * 可重入锁，也叫做递归锁，指的是同一线程 外层函数获得锁之后 ，内层递归函数仍然有获取该锁的代码，但不受影响。
+    在JAVA环境下 ReentrantLock 和synchronized 都是 可重入锁
+
+    不可重入锁：意思是，外层方法以及获取锁了，里层方法如果需要锁的话，
+    可以直接用了，不然一个等着另一个会死锁，像下面那个print与add
+ */
+public class _08_01_TestReentrantLock {
+
+    Lock lock = new ReentrantLock();
+    public void print() throws InterruptedException {
+        System.out.println("do print");
+        lock.lock();
+        doAdd();
+        System.out.println("unlock do print");
+        lock.unlock();
+    }
+    public void doAdd() throws InterruptedException {
+        System.out.println("do add");
+        lock.lock();
+        System.out.println("unlock do add");
+        //do something
+        lock.unlock();
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        _08_01_TestReentrantLock testLock = new _08_01_TestReentrantLock();
+        testLock.print();
+    }
+}
